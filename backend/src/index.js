b289f00d206bbf12579cfdd5c9eb21d5cef1f126
Mehul Uttam <mehulsavio@gmail.com>
2024-12-23 22:58:24 +0530

    add frontend pages and backend message routes for chat functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
@@ -11,7 +12,7 @@ app.use(cookieParser());
 
 const port = process.env.PORT;
 app.use("/api/auth", authRoutes);
-
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
