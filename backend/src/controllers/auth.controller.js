b120387e8ee40a939d8297fbf6858a36ad090f9f
Mehul Uttam <mehulsavio@gmail.com>
2024-12-23 13:22:52 +0530

    initialize backend and frontend structure with basic configurations and models

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrpyt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrpyt.genSalt(10);
+    const hashedPassword = await bcrpyt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("Login route");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout route");
+};
---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
+app.use("/api/auth", authRoutes);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  connectDB();
+});
