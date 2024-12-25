f445bacc4c50f868c3b0c89a3715b27ea2e6211d
Mehul Uttam <mehulsavio@gmail.com>
2024-12-25 23:30:46 +0530

    update backend and frontend configurations for production; add Dockerfile, update CORS and API URLs

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://chathub-dev.vercel.app", credentials: true }));
 
 const port = process.env.PORT;
 app.use("/api/auth", authRoutes);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://chathub-backend.vercel.app/api",
     withCredentials: true,
     
 });
